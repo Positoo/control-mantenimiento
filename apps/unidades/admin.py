ae462291b29a581df43ebdd2e97cb5b6636a6a2a
Ariel C <acremona66@gmail.com>
2026-09-20 16:54:17 -0300

    Agrega maestro de Unidades

--- apps/unidades/admin.py
+++ apps/unidades/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import UnidadDeNegocio, Alcance
+from .models import UnidadDeNegocio, Alcance, Unidad
 
 
 @admin.register(UnidadDeNegocio)
@@ -14,3 +14,29 @@ class AlcanceAdmin(admin.ModelAdmin):
     list_display = ("id", "nombre", "activo")
     list_filter = ("activo",)
     search_fields = ("nombre",)
+
+
+@admin.register(Unidad)
+class UnidadAdmin(admin.ModelAdmin):
+    list_display = (
+        "id_interno",
+        "movil",
+        "dominio",
+        "marca",
+        "modelo",
+        "unidad_de_negocio",
+        "alcance",
+    )
+
+    search_fields = (
+        "movil",
+        "dominio",
+        "marca",
+        "modelo",
+    )
+
+    list_filter = (
+        "unidad_de_negocio",
+        "alcance",
+        "activo",
+    )
--- apps/unidades/models.py
+++ apps/unidades/models.py
@@ -16,3 +16,48 @@ class Alcance(models.Model):
 
     def __str__(self):
         return self.nombre
+
+
+#Unidades este es el modelo para crear las distintas unidades
+class Unidad(models.Model):
+    id_interno = models.AutoField(primary_key=True)
+    movil = models.CharField(max_length=20, unique=True, blank=True)
+    dominio = models.CharField(max_length=20, unique=True)
+    marca = models.CharField(max_length=100, blank=True)
+    modelo = models.CharField(max_length=100, blank=True)
+    nro_chasis = models.CharField(max_length=100, blank=True)
+    nro_motor = models.CharField(max_length=100, blank=True)
+
+    capacidad_m3 = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        null=True,
+        blank=True,
+    )
+
+    capacidad_tn = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        null=True,
+        blank=True,
+    )
+
+    unidad_de_negocio = models.ForeignKey(
+        UnidadDeNegocio,
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+    )
+
+    alcance = models.ForeignKey(
+        Alcance,
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+    )
+
+    observaciones = models.TextField(blank=True)
+    activo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.id_interno} - {self.dominio}"   #aqui agregar self.movil cuando tengamos definido por movil
