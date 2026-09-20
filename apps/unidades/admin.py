2b4c4da22db821f2c91e4a3df8b8cb10e1272b1c
Ariel C <acremona66@gmail.com>
2026-09-20 16:04:49 -0300

    Agrega maestro de Alcance

--- apps/unidades/admin.py
+++ apps/unidades/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 
-from .models import UnidadDeNegocio
+from .models import UnidadDeNegocio, Alcance
 
 
 @admin.register(UnidadDeNegocio)
 class UnidadDeNegocioAdmin(admin.ModelAdmin):
+    list_display = ("id", "nombre", "activo")
+    list_filter = ("activo",)
+    search_fields = ("nombre",)
+
+@admin.register(Alcance)
+class AlcanceAdmin(admin.ModelAdmin):
     list_display = ("id", "nombre", "activo")
     list_filter = ("activo",)
     search_fields = ("nombre",)
--- apps/unidades/models.py
+++ apps/unidades/models.py
@@ -7,3 +7,12 @@ class UnidadDeNegocio(models.Model):
 
     def __str__(self):
         return self.nombre
+
+
+#Alcance. puede ser H-distribucion, H-Produccion, Corralon, etc.
+class Alcance(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+    activo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
